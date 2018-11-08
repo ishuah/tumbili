f23fc45083f57c5fe9a600b5a48e2c43e8bdd761
ishuah <kariuki@ishuah.com>
2018-11-08 16:52:49 +0300

    Added ascii function

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 build:
 	dep ensure
 	env GOOS=linux go build -ldflags="-s -w" -o bin/echo echo/main.go
+	env GOOS=linux go build -ldflags="-s -w" -o bin/ascii ascii/main.go
---
+++ ascii/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/common-nighthawk/go-figure"
+)
+
+type AsciiRequest struct {
+	Text string `json:"text"`
+}
+
+type AsciiResponse struct {
+	Text   string `json:"text"`
+	Art    string `json:"art"`
+	Length int    `json:"length"`
+}
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var asciiRequest AsciiRequest
+	err := json.Unmarshal([]byte(request.Body), &asciiRequest)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 400}, nil
+	}
+	myFigure := figure.NewFigure(asciiRequest.Text, "big", true)
+	asciiResponse := AsciiResponse{
+		Text: asciiRequest.Text,
+		Art:  myFigure.String(),
+	}
+	response, err := json.Marshal(asciiResponse)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
+	}
+	return events.APIGatewayProxyResponse{Body: string(response), StatusCode: 200}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
